95efd44a93e2fc766004e0f8eca861dd188ec43a
Animesh Nayak <animesh676@gmail.com>
2026-02-17 20:36:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let taskInput = document.getElementById("taskInput");
+let button = document.getElementById("addbtn");
+let ul = document.getElementById("todoContainer");
+
+// Adding tasks to To-do array
+let taskArr = [];
+button.addEventListener(("click"), () => {
+
+    taskArr = taskInput.value;
+    console.log(taskArr);
+
+    //  Creating To-do nodes
+    let toDo = document.createElement("li");
+    function toDoNodes ( ) {
+        toDo.innerText = taskArr;
+    }
+    
+    // rendering the lis to DOM
+    function render ( ) {
+        ul.appendChild(toDo);
+        taskInput.value = "";
+    }
+
+    /* -------------- Adding additonal functionalites ---------------- */
+
+    // Removing to-do node on User's request
+    function removeToDo ( ) {
+        const removeBtn = document.createElement("button");
+        removeBtn.innerText = "❎";
+        toDo.appendChild(removeBtn);
+
+        removeBtn.addEventListener(("click"), () => {
+            toDo.remove();
+        });
+    }
+
+    // 
+    toDoNodes( );
+    render ( );
+    removeToDo( );
+});
+
+
+
